8d092a506436cf0c56ca40574af5d2ad6436e146
malekbenz <malekbenz@gmail.com>
2016-08-17 14:06:28 +0100

    PizzaRio First commit

---
+++ app/app.js
@@ -0,0 +1,6 @@
+angular.module("pizzaApp",[])
+        .controller("main",function(){
+            this.articles   = ['1','2','3','4','5','6','7','8'];
+            this.tables     = ['1','2','3','4','5','6','7','8','9','10'];
+
+        });
